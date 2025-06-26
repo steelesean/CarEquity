ecd960347690db21e62c993b7186c87e7ba970e1
steelesean <sean_mcharg@hotmail.co.uk>
2025-06-26 16:16:16 +0100

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>UK Car Equity Checker</h1>
+      <p>Welcome! This is your Apple-inspired car equity app.</p>
+    </div>
+  );
+}
+
+export default App;
